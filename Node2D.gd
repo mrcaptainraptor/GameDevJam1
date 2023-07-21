a40c1f84b0a7985bda0b515174a6c0800e419430
MrNonesCreations <mr.itsmee@gmail.com>
2023-07-20 19:47:00 -0500

    character

--- Node2D.gd
+++ Node2D.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-# Test does this work :o
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("Test LOL")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
